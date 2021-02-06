6c84b57675b20bd727865e1fccc74c05d5631a20
sossw1 <sossw1@gmail.com>
2021-02-05 19:31:50 -0500

    Added schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Big Mac', false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Bacon Cheeseburger', false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Double Decker', false);
