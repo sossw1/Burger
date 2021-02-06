47e5dfc7338d583652dca1147d401ed704858067
sossw1 <sossw1@gmail.com>
2021-02-06 09:21:28 -0500

    orm object contains selectAll, insertOne and updateOne functions

--- config/orm.js
+++ config/orm.js
@@ -1,15 +1,5 @@
 const connection = require('./connection');
 
-/*
-    selectAll : SELECT * FROM table (chosen) then call cb on result
-    
-    insertOne : INSERT INTO table (stringified col) VALUES (given # ?'s)
-        where ?'s later filled by key-value pairs from vals, then error or cb
-    
-    updateOne : UPDATE table (chosen) SET (objColVals in sql format)
-        WHERE condition, then error or cb
-*/
-
 // Given num, return a stringified array of '?'s of length num
 const printQuestionMarks = (num) => {
     let arr = [];
@@ -19,7 +9,7 @@ const printQuestionMarks = (num) => {
     return arr.toString();
 }
 
-// given object, return stringified array of key-value pairs
+// Given object, return stringified array of key-value pairs
 const objToSql = (obj) => {
     const arr = [];
     for(const key in obj) {
@@ -34,3 +24,46 @@ const objToSql = (obj) => {
     return arr.toString();
 }
 
+// Object-Relational Mapping
+const orm = {
+
+    // Select all rows from given table, then execute callback
+    selectAll(table, cb) {
+        const queryString = `SELECT * FROM ${table};`;
+        connection.query(queryString, (err, result) => {
+            if(err) throw err;
+            cb(result);
+        });
+    },
+
+    // Insert values into columns in a given table, then execute callback
+    insertOne (table, cols, vals, cb) {
+        let queryString = `INSERT INTO ${table}`;
+        queryString += ' (';
+        queryString += cols.toString();
+        queryString += ') ';
+        queryString += 'VALUES (';
+        queryString += printQuestionMarks(vals.length);
+        queryString += ') ';
+
+        connection.query(queryString, vals, (err, result) => {
+            if(err) throw err;
+            cb(result);
+        });
+    },
+
+    /* Update the given columns where a condition is met in a given table,
+       then execute callback */
+    updateOne (table, objColVals, condition, cb) {
+        let queryString = `UPDATE ${table}`;
+        queryString += ' SET ';
+        queryString += objToSql(objColVals);
+        queryString += ' WHERE ';
+        queryString += condition;
+
+        connection.query(queryString, (err, result) => {
+            if(err) throw err;
+            cb(result);
+        });
+    }
+}
