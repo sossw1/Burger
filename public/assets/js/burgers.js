b900f3de7150d68aae5db76f41a70b2631dcda72
sossw1 <sossw1@gmail.com>
2021-02-08 19:02:14 -0500

    POST fetch

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -29,4 +29,27 @@ document.addEventListener('DOMContentLoaded', (event) => {
             });
         });
     }
+
+    const createBurgerBtn = document.getElementById('create-form');
+    if(createBurgerBtn) {
+        createBurgerBtn.addEventListener('submit', (e) => {
+            e.preventDefault();
+            const newBurger = {
+                burger_name: document.getElementById('bu').value.trim(),
+                devoured: false
+            }
+            fetch('/api/burgers', {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(newBurger)
+            }).then(() => {
+                document.getElementById('bu').value = '';
+                console.log('Created a new burger!')
+                location.reload();
+            });
+        });
+    }
 });
