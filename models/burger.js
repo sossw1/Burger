b4b995d3fb7a6ef2ab0fe1437dca57c0f051a75f
sossw1 <sossw1@gmail.com>
2021-02-08 19:00:00 -0500

    Fixed model insert method name insertOne

--- models/burger.js
+++ models/burger.js
@@ -6,7 +6,7 @@ const burger = {
         orm.selectAll('burgers', (res) => cb(res));
     },
     insertOne(cols, vals, cb) {
-        orm.create('burgers', cols, vals, (res) => cb(res));
+        orm.insertOne('burgers', cols, vals, (res) => cb(res));
     },
     updateOne(objColVals, condition, cb) {
         orm.updateOne('burgers', objColVals, condition, (res) => cb(res));
